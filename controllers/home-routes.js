b660372fd2faacb894e541af49ed83a9484868dc
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-12 21:57:30 -0700

    display home content rather than login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,13 @@
+const { restore } = require('../models/user');
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.render('main');
 });
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
 
 module.exports = router;
